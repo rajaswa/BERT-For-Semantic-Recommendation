d3393b63acb6fb930d4494194de4724f9ba027b9
Siddhant Mahurkar <45026019+sidmahurkar@users.noreply.github.com>
2019-10-26 18:16:04 +0530

    Update run.py

--- flaskapp/run.py
+++ flaskapp/run.py
@@ -12,13 +12,25 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 def home():
 	return render_template('home.html')
 
-
+# Normal inputform route
 @app.route("/inputform",methods=['GET','POST'])
 def inputform():
 	form = QueryForm()
 	if form.validate_on_submit():
 		flash('Query Submitted!','success')
 		return redirect(url_for('home'))
+	return render_template('inputform.html', form = form)
+
+# Route with length option 
+# render inside render.
+@app.route("/inputform",methods=['GET','POST'])
+def inputform():
+	form = QueryForm()
+	if form.validate_on_submit():
+		abc = form.query.data
+		len_query = len(abc)
+		# flash('Query Submitted!','success')
+		return render_template('len.html', len_query = len_query)
 	return render_template('inputform.html', form = form)
 
 

a6c1d87d13747632060ed93728eb9f182d568032
Siddhant Mahurkar <45026019+sidmahurkar@users.noreply.github.com>
2019-11-28 02:03:12 +0530

    Add files via upload

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 class QueryForm(FlaskForm):
-	query = StringField('Query', validators = [DataRequired(), Length(min=3)])
+	query = TextAreaField('Query', validators = [DataRequired(), Length(min=3)])
 	submit = SubmitField('Submit Query')
+
--- flaskapp/run.py
+++ flaskapp/run.py
@@ -1,5 +1,18 @@
+# Flask Imports
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import QueryForm
+
+# Framework Imports
+import pandas as pd
+import numpy as np
+
+from nltk.cluster import KMeansClusterer
+import nltk  
+
+from sklearn import cluster
+from sklearn import metrics
+
+from utils import *
 
 
 app = Flask(__name__)
@@ -12,26 +25,38 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 def home():
 	return render_template('home.html')
 
-# Normal inputform route
+
+# @app.route("/inputform",methods=['GET','POST'])
+# def inputform():
+# 	form = QueryForm()
+# 	if form.validate_on_submit():
+# 		flash('Query Submitted!','success')
+# 		return redirect(url_for('home'))
+# 	return render_template('inputform.html', form = form)
+
+
 @app.route("/inputform",methods=['GET','POST'])
 def inputform():
 	form = QueryForm()
 	if form.validate_on_submit():
-		flash('Query Submitted!','success')
-		return redirect(url_for('home'))
+
+		query = form.query.data
+		df = pd.read_pickle('cmu_data_phrase.pkl')
+
+		#Get top-n entries by score ranking
+		df['score'] = df.apply(lambda row: get_score(query, row.noun_phrase, row.sentiment, alpha=0.95), axis = 1) 
+		df_topn = df.nlargest(500, 'score')
+		df_topn.reset_index(inplace=True)
+
+
+		recommendations = get_recommendation(query, df, df_topn, n=500, NUM_CLUSTERS=10)
+
+
+		# Can uncomment if we want to query submission feedback
+		# flash('Query Submitted!','success')
+		return render_template('output.html', recommendations = recommendations)
 	return render_template('inputform.html', form = form)
 
-# Route with length option 
-# render inside render.
-@app.route("/inputform",methods=['GET','POST'])
-def inputform():
-	form = QueryForm()
-	if form.validate_on_submit():
-		abc = form.query.data
-		len_query = len(abc)
-		# flash('Query Submitted!','success')
-		return render_template('len.html', len_query = len_query)
-	return render_template('inputform.html', form = form)
 
 
 
